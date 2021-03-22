e3edb359e96ecfd9df21f0a128240a3cf103b61e
ElderMicrobe889 <you@example.com>
2021-03-22 14:06:02 -0400

    More Nav Stuff And Some Colors

--- static/shared/shared.js
+++ static/shared/shared.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    $(".nav-link").each(function (index, value) {
+        value = $(value)
+        if (document.title.endsWith(value.text())) {
+            value.addClass("current");
+        }
+    });
     $(".nav-button").click(function() {
         let navLinkContainer = $(".nav-link-container");
         if (!navLinkContainer.hasClass("show")) {
