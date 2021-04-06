008941f7ed6f6497d76d08e467241ebd54d04019
ElderMicrobe889 <you@example.com>
2021-04-06 12:05:19 -0400

    Upcoming Events Section Draft

--- static/base/base.js
+++ static/base/base.js
@@ -2,18 +2,17 @@ $(document).ready(function() {
     $(".nav-link").each(function (index, value) {
         value = $(value)
         if (document.title.endsWith(value.text())) {
-            value.addClass("current");
+            value.addClass("current-nav-link");
         }
     });
     $(".nav-button").click(function() {
+        $("body").toggleClass("menu-shown");
         let navLinkContainer = $(".nav-link-container");
-        if (!navLinkContainer.hasClass("show")) {
-            navLinkContainer.scrollTop(0);
-        }
-        navLinkContainer.toggleClass("show");
+        navLinkContainer.scrollTop(0);
+        navLinkContainer.toggleClass("menu-shown");
         $(".nav-link").each(function (index, value) {
             setTimeout(function() {
-                $(value).toggleClass("show");
+                $(value).toggleClass("menu-shown");
             }, index*100);
         });
     });
