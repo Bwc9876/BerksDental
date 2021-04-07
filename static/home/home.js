8ed54e5084fb78aa387fac30c8e0be3972636d9b
ElderMicrobe889 <you@example.com>
2021-04-07 11:50:53 -0400

    Upcoming Events Stuff

---
+++ static/home/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".event-card").click(function() {
+        let hadClass = $(this).hasClass("focused-event-card");
+        $(".event-card").each(function (index, value) {
+            $(value).removeClass("focused-event-card");
+        });
+        if (!hadClass) {
+            $(this).addClass("focused-event-card");
+        }
+    });
+});
