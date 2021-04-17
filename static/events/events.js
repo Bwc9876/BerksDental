fad7464f906d413d475e8096a67637fcb4355524
ElderMicrobe889 <you@example.com>
2021-04-17 00:30:06 -0400

    Calender Stuff

--- static/events/events.js
+++ static/events/events.js
@@ -1,6 +1,6 @@
 function getEvents(calendar_tile) {
     let events = [];
-    $(calendar_tile).children(".event").each((index, child) => {
+    calendar_tile.children(".event").each((index, child) => {
         events.push(JSON.parse($(child).text()));
     });
     return events;
@@ -15,6 +15,24 @@ $(document).ready(function () {
             $(value).removeClass("focused-tile");
         });
         $(this).addClass("focused-tile");
-        console.log(getEvents(this));
+        let events = getEvents($(this));
+        let eventsToday = (events.length !== 0);
+        $(".front-event-card").each(function (index, value) {
+            if (index === 0) { return; }
+            $(value).detach();
+        });
+        $("#event-list-title").text(eventsToday? "Events on " + events[0]["startDate"] : "There are no events that day.");
+        for (let i = events.length-1; i >= 0; i--) {
+            let event = events[i];
+            let sameDay = (event["startDate"] === event["endDate"]);
+            let eventCard = `<div class="front-event-card">
+        <h2>${event["name"]}${sameDay?"":" Starts"}</h2>
+        <p class="h4">${sameDay?`${event["startTime"]} - ${event["endTime"]}`:event["startTime"]}</p>
+        <p class="h4">${event["virtual"]?`<a href="${event["link"]}">${event["link"]}</a>`:event["location"]}</p>
+        <p class="event-front-description h4">${event["description"]}</p>
+    </div>`;
+            $(".event-cards-calendar").append(eventCard);
+        }
     });
+    $(".calendar-today").click();
 });
