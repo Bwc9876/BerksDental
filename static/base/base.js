eb53fb8f21258959c7f8b03402dad3ed3ed20efd
ElderMicrobe889 <you@example.com>
2021-04-24 15:46:29 -0400

    Started Revision (Home Page)

--- static/base/base.js
+++ static/base/base.js
@@ -1,21 +1,13 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $(".nav-link").each(function (index, value) {
         value = $(value)
         if (document.title.endsWith(value.text())) {
-            value.addClass("current-nav-link");
+            value.addClass("nav-link-current");
         }
     });
-    $(".nav-button").click(function () {
-        const body = $("body");
-        body.toggleClass("menu-shown");
-        $(".nav-button").text(body.hasClass("menu-shown")? "Close": "Menu")
-        let navLinkContainer = $(".nav-link-container");
-        navLinkContainer.scrollTop(0);
-        navLinkContainer.toggleClass("menu-shown");
-        $(".nav-link").each(function (index, value) {
-            setTimeout(function () {
-                $(value).toggleClass("menu-shown");
-            }, index * 100);
-        });
+    $(".navbar-burger").click(function() {
+        $(this).toggleClass("is-active");
+        $(this).toggleClass("has-text-danger-dark");
+        $(".navbar-menu").toggleClass("is-active");
     });
 });
