f004d8d0d8b8a71d0102f07ed1219b6c8cb11399
b_crocker <bwc9876@gmail.com>
2021-05-13 14:40:51 -0400

    fixed issue on calendar when there is no today

--- static/events/calendar.js
+++ static/events/calendar.js
@@ -1,12 +1,22 @@
-$(document).ready(function () {
-    $(".calendar-tile").click(function () {
-        $(".calendar-tile").each(function (index, value) {
-            let tile = $(value);
-            tile.removeClass("focused-tile");
-            $(`.${tile.attr("data-berks-dental-event-date")}`).css("display", "none");
-        });
-        $(this).addClass("focused-tile");
-        $(`.${$(this).attr("data-berks-dental-event-date")}`).css("display", "grid");
+function evaluateTiles(){
+    $(".calendar-tile").each(function (index, value) {
+        let tile = $(value);
+        tile.removeClass("focused-tile");
+        $(`.${tile.attr("data-berks-dental-event-date")}`).css("display", "none");
     });
-    $(".calendar-today").click();
+    $(this).addClass("focused-tile");
+    $(`.${$(this).attr("data-berks-dental-event-date")}`).css("display", "grid");
+}
+
+$(document).ready(function () {
+    let today = $(".calendar-today");
+    let tiles = $(".calendar-tile");
+    tiles.click(evaluateTiles);
+    if (today.length === 0)
+    {
+        evaluateTiles.bind(tiles.first())();
+    } else {
+        today.click();
+    }
+
 });
