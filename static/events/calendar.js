43532bd438d996bc14565f6c86fce5017869bb83
ElderMicrobe889 <you@example.com>
2021-05-13 20:14:51 -0400

    FINAL DRAFT CALENDAR PAGE

--- static/events/calendar.js
+++ static/events/calendar.js
@@ -1,22 +1,21 @@
-function evaluateTiles(){
-    $(".calendar-tile").each(function (index, value) {
-        let tile = $(value);
-        tile.removeClass("focused-tile");
-        $(`.${tile.attr("data-berks-dental-event-date")}`).css("display", "none");
-    });
-    $(this).addClass("focused-tile");
-    $(`.${$(this).attr("data-berks-dental-event-date")}`).css("display", "grid");
-}
-
 $(document).ready(function () {
-    let today = $(".calendar-today");
     let tiles = $(".calendar-tile");
-    tiles.click(evaluateTiles);
-    if (today.length === 0)
-    {
-        evaluateTiles.bind(tiles.first())();
-    } else {
-        today.click();
-    }
-
+    let today = $(".calendar-today");
+    let focused = today.length === 0 ? tiles.first() : today;
+    tiles.click(function() {
+        tiles.each(function (index, value) {
+            let tile = $(value);
+            let id = tile.attr("data-berks-dental-event-date")
+            tile.removeClass("focused-tile");
+            $(`.${id}`).css("display", "none");
+        });
+        $(this).addClass("focused-tile");
+        let id = $(this).attr("data-berks-dental-event-date")
+        $(`.${id}`).each(function (index, value) {
+            let element = $(value);
+            let setType = element.prop("tagName") === "P" ? "initial" : "grid";
+            element.css("display", setType);
+        });
+    });
+    focused.click();
 });
