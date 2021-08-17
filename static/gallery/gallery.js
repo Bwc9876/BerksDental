323571131afc4aeb16910423845505190e9b333a
ElderMicrobe889 <pycharm@pycharm.pycharm>
2021-08-16 20:27:25 -0400

    Gallery Redone

--- static/gallery/gallery.js
+++ static/gallery/gallery.js
@@ -1,4 +1,4 @@
-function requestPage(number) {
+function requestNextImageSet(number) {
     return new Promise((resolve) => {
         $.post("/gallery-page/", {'page': number}, (data) => {
             resolve(data);
@@ -9,26 +9,26 @@ function requestPage(number) {
 $(document).ready(function() {
     let current = 1;
     let grid = $(".image-grid");
+    let cell = $("#load-images-cell")
     let button = $("#load-images-button");
     button.click(function() {
         current++;
-        $("#load-images-button").addClass("is-loading");
-        requestPage(current).then(function(results) {
+        button.addClass("is-loading");
+        requestNextImageSet(current).then(function(results) {
             let photos = results["photos"];
             for (let photo of photos) {
                 let appendString = `
-                <div class="image-grid-item-outer-wrapper">
-                    <a href="${photo["link"]}" class="image-grid-item-inner-wrapper">
-                        <img src="${photo["src"]}" alt="${photo["alt"]}" />
+                <div class="image-grid-cell">
+                    <a href="${photo["link"]}" class="image-grid-item-wrapper">
+                        <img class="image-grid-item" src="${photo["src"]}" alt="${photo["alt"]}" />
                     </a>
                 </div>`;
-                button.detach()
+                cell.detach()
                 $(appendString).appendTo(grid);
-                grid.append(button)
             }
-            $("#load-images-button").removeClass("is-loading");
-            if (!results["hasNext"]) {
-                button.detach();
+            if (results["hasNext"]) {
+                grid.append(cell);
+                button.removeClass("is-loading");
             }
         });
     });
