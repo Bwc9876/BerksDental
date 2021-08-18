23d1c909fb630c3eb69e3e95d0a0e72868c13bd6
ElderMicrobe889 <pycharm@pycharm.pycharm>
2021-08-18 19:08:19 -0400

    Events Pages Redone

--- static/events/calendar.js
+++ static/events/calendar.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
-    let tiles = $(".calendar-tile");
-    let today = $(".calendar-today");
-    let focused = today.length === 0 ? tiles.first() : today;
+    let tiles = $(".in-month");
+    let today = $(".is-today");
+    let selected = today.length === 0 ? tiles.first() : today;
     tiles.click(function() {
         tiles.each(function (index, value) {
             let tile = $(value);
-            let id = tile.attr("data-berks-dental-event-date")
-            tile.removeClass("focused-tile");
-            $(`.${id}`).css("display", "none");
+            let id = tile.attr("data-berks-dental-event-date");
+            tile.removeClass("is-selected");
+            if (id !== undefined && id !== "") {
+                $(`.${id}`).each(function (index, value) {
+                    $(value).removeClass("is-selected");
+                });
+            }
         });
-        $(this).addClass("focused-tile");
+        $(this).addClass("is-selected");
         let id = $(this).attr("data-berks-dental-event-date")
         $(`.${id}`).each(function (index, value) {
-            let element = $(value);
-            let setType = element.prop("tagName") === "P" ? "initial" : "grid";
-            element.css("display", setType);
+            $(value).addClass("is-selected");
         });
     });
-    focused.click();
+    selected.click();
 });
